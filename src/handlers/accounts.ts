6ab6b2e19c3c6d706ac8781e997a377f7523717b
quint <quint@neflyx.com>
2024-08-21 12:15:55 +0200

    1 acc page per block, delegated amount fix, cleaning pagekeys

--- src/handlers/accounts.ts
+++ src/handlers/accounts.ts
@@ -1,7 +1,8 @@
 import { SubstrateBlock } from "@subql/types";
-import { Account, DelegateBalance } from "../types";
+import {Account, DelegateBalance} from "../types";
 import { ZERO } from "../utils/consts";
-import { accountPageKeys } from "../utils/accountPageKeys";
+
+export let currentPageKey: string | undefined = undefined;
 
 const removeAllAccounts = async () => {
   const allRecords = await store.getByFields("Account", []);
@@ -9,30 +10,38 @@ const removeAllAccounts = async () => {
   await store.bulkRemove("Account", allIds);
 };
 
+const removePageKeys = async () => {
+  let count = 0;
+  while (true) {
+    const records = await store.getByFields("Account", []);
+    const ids = records.map(({ id }) => id).filter((id) => id.startsWith('0x26aa'));
+    if (ids.length === 0){
+      break;
+    }
+      count += ids.length;
+      await store.bulkRemove("Account", ids);
+  }
+};
+
 export async function fetchAccounts(block: SubstrateBlock): Promise<void> {
+
   const height = block.block.header.number.toBigInt();
 
   logger.info(`Fetching accounts at block #${height}`);
-  // Fetch all DelegateBalance entries for staking module
-  //todo: this returns only 100 records
-  const allDelegateBalances = await DelegateBalance.getByFields([]);
-
-  // Create a map of account addresses to their staked balances
-  const stakedBalancesMap = new Map<string, bigint>();
-  for (const delegateBalance of allDelegateBalances) {
-    const currentBalance = stakedBalancesMap.get(delegateBalance.account) || ZERO;
-    stakedBalancesMap.set(delegateBalance.account, currentBalance + delegateBalance.amount);
-  }
-
-  // removeAllAccounts();
-  // logger.info(`#${height}: fetchAccounts`);
 
   const accountsIterator = getAccountsIterator(block);
   const entities: Account[] = [];
 
   for await (const [address, freeBalance] of accountsIterator) {
     const freeBalanceBigInt = BigInt(freeBalance);
-    const stakedBalance = stakedBalancesMap.get(address) || ZERO;
+    const stakedBalances = (await DelegateBalance.getByAccount(address))?.map(val => val.amount);
+    let stakedBalance = ZERO;
+    if(stakedBalances){
+      stakedBalances.forEach(balance => {
+        stakedBalance += balance
+      });
+    }
+
     const totalBalance = freeBalanceBigInt + stakedBalance;
 
     entities.push(
@@ -47,49 +56,32 @@ export async function fetchAccounts(block: SubstrateBlock): Promise<void> {
       })
     );
   }
-  logger.info(`#${height}: acc length ${entities.length}`);
+
   await store.bulkCreate("Account", entities);
 }
 
 async function* getAccountsIterator(block: SubstrateBlock): AsyncGenerator<[string, string]> {
   if (!unsafeApi) throw new Error("API not initialized");
 
-  const blockheight = block.block.header.number.toNumber();
   const hash = block.block.header.hash.toString();
   const apiAt = await unsafeApi.at(hash);
 
-  //adjust if necessary
-  const chunkIdx = Math.floor((blockheight % 100) / 1);
-  const startKeys = [...accountPageKeys];
   const pageSize = 1000;
 
+  const accEntries = await apiAt.query.system.account.entriesPaged({
+    args: [],
+    pageSize,
+    startKey: currentPageKey});
 
-  //adjust if necessary
-  let spreadCallsOverAmnt = 100;
-  const chunkSize = Math.ceil(startKeys.length / spreadCallsOverAmnt);
+  if (accEntries.length === 0 || accEntries.length < 1000){
+    //no more entries, start at beginning...
+    currentPageKey = undefined;
+    return;
+  }
 
-  const startIndex = chunkIdx * chunkSize;
-
-  const pagesToDoThisBatch = startKeys.slice(startIndex, startIndex+chunkSize);
-
-  let calls = pagesToDoThisBatch.map(startKey => apiAt.query.system.account.entriesPaged({
-      args: [],
-      pageSize,
-      startKey}));
-
-    let pagedResults = await Promise.all(calls);
-    for (const entries of pagedResults) {
-      if (entries.length === 0){
-        // spreadCallsOverAmnt = 0;
-        break;
-      }
-//todo: dynamically add more pagekeys if needed
-//       if (entries.length === 1000 &&){
-//       }
-
-      for (const [key, accountInfo] of entries) {
-        yield [key.args[0].toString(), accountInfo.data.free.toString()];
-      }
-    }
-  // }
+  for (let i = 0; i < accEntries.length - 1; i++) {
+    const [key, accountInfo] = accEntries[i];
+    yield [key.args[0].toString(), accountInfo.data.free.toString()];
+  }
+  currentPageKey = accEntries[accEntries.length - 1][0].toString();
 }
